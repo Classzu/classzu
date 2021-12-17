e0ce143f201375311136c8081cdc5df973abe166
Leandro <leandro83g.development@gmail.com>
2021-12-17 18:14:50 +0900

    feat: add sorage config

--- src/config/storage.ts
+++ src/config/storage.ts
@@ -1,6 +1,19 @@
+const getNameWithPrefix = (name: string) => {
+    const prefix = "classzu.";
+    return prefix + name;
+}
+
 const Storage = {
     local: {
-        name: 'classzu'
+        name: 'classzu',
+        tables: {
+            files: {
+                name: getNameWithPrefix("files")
+            },
+            directories: {
+                name: getNameWithPrefix("directories")
+            }
+        }
     }
 }
 
