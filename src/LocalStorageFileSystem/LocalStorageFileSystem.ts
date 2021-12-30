2a4e03ae41168808248e000e453cde3bb3fc58a5
Leandro <leandro83g.development@gmail.com>
2021-12-30 13:33:20 +0900

    feat: add new Dir LocalStorageFileSystem

---
+++ src/LocalStorageFileSystem/LocalStorageFileSystem.ts
@@ -0,0 +1,12 @@
+import Classzu from "..";
+
+class LocalStorageFileSystem {
+    public classzu: Classzu
+    constructor(classzu: Classzu) {
+        this.classzu = classzu;
+    }
+    
+}
+
+
+export default LocalStorageFileSystem;
